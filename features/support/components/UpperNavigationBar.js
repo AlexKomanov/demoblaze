a07519fce429aa071bc9b585a41588520a0ed103
Alex Komanov <akomanov@seatgeek.com>
2021-02-24 00:40:46 +0200

    Added mapping file for the navigation bar

---
+++ features/support/components/UpperNavigationBar.js
@@ -0,0 +1,11 @@
+const upperNavigationBar = {
+    'Home': '.navbar-nav.ml-auto [href="index.html"]',
+    'Contact': '.navbar-nav.ml-auto [data-target="#exampleModal"]',
+    'About us': '.navbar-nav.ml-auto [data-target="#videoModal"]',
+    'Cart': '#cartur',
+    'Log in': '#login2',
+    'Sign Up': '#signin2',
+
+};
+
+module.exports = upperNavigationBar;
