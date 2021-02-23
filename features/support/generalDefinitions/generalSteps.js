f8665d08287b1d0d6045d84aeb8980bcee411c99
Alex Komanov <akomanov@seatgeek.com>
2021-02-24 01:12:49 +0200

    Added first test steps for a Sign Up test

---
+++ features/support/generalDefinitions/generalSteps.js
@@ -0,0 +1,14 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const utils = require('../../lib/utils');
+const upperNavigationBar = require('../components/UpperNavigationBar');
+
+Given('I open a main page', async function () {
+	await this.page.goto('https://www.demoblaze.com/index.html');
+});
+
+When(
+	'I clicked a {string} button on the Main Page',
+	async function (buttonName) {
+		utils.click(this.page, upperNavigationBar[buttonName]);
+	}
+);
---
+++ features/tests/signUp/signUp.feature
@@ -0,0 +1,8 @@
+Feature: Feature name
+
+    Feature Description
+
+    Scenario: Scenario name
+
+    Given I open a main page
+    When I clicked a "Sign up" button on the Main Page
