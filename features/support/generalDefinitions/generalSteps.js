4feefdd0685059e172ef07612f190da921ddcbb6
Alex Komanov <akomanov@seatgeek.com>
2021-02-24 13:14:35 +0200

    Refactoring of generalSteps file

--- features/support/generalDefinitions/generalSteps.js
+++ features/support/generalDefinitions/generalSteps.js
@@ -1,9 +1,18 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const utils = require('../../lib/utils');
 const upperNavigationBar = require('../components/UpperNavigationBar');
+const expect = require('chai').expect;
+
+let alertMessage = '';
 
 Given('I open a main page', async function () {
+	this.page.on('dialog', async dialog => {		
+		alertMessage = await dialog.message();
+		await dialog.accept();
+	});
 	await this.page.goto('https://www.demoblaze.com/index.html');
+	
+	
 });
 
 When(
@@ -12,3 +21,12 @@ When(
 		await utils.click(this.page, upperNavigationBar[buttonName]);
 	}
 );
+
+When("I print a message to the console", async function(){
+	console.log("Message: " + alertMessage)
+})
+
+When("I validate that alert message contains {string} and submit", async function(expectedMessage){	
+	console.log("Message 2: " + alertMessage)
+	expect(alertMessage).to.contain(expectedMessage);
+})
